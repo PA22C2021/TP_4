0af5f2692b263e348f41a07962f7fa7c792fa245
femansilla <fmansilla@roemmers.com.ar>
2021-10-08 21:08:35 -0300

    datos de conexion ok

--- app/src/main/java/com/example/tp_4/conexion/DataDB.java
+++ app/src/main/java/com/example/tp_4/conexion/DataDB.java
@@ -4,9 +4,9 @@ public class DataDB {
     //Información de la BD
     public static String host="sql10.freesqldatabase.com";
     public static String port="3306";
-    public static String nameBD="sql10305519";
-    public static String user="sql10305519";
-    public static String pass="YAzn6xkR7R";
+    public static String nameBD="sql10442660";
+    public static String user="sql10442660";
+    public static String pass="5vu3EXhTp4";
 
     //Información para la conexion
     public static String urlMySQL = "jdbc:mysql://" + host + ":" + port + "/"+nameBD;
